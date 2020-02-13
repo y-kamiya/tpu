825ed617cc50b9417c4f67b83bf2dd277f451b31
Yuji Kamiya <y.kamiya0@gmail.com>
2020-02-13 12:55:30 +0900

    refactor copy_images not to use gxargs

--- scripts/copy_images.sh
+++ scripts/copy_images.sh
@@ -10,6 +10,18 @@ if [ ! -d $from ]; then
     exit 1
 fi
 
-cat $classlist | gxargs -I{} bash -c "mkdir -p $to/{}"
-cat $classlist | gxargs -n1 -I{} -P8 bash -c "find $from -maxdepth 3 -name \"{}@*\" | shuf | head -n $count | gxargs -I@ -P8 cp @ $to/{}/"
-#head -n $count | gxargs -I@ -P8 echo @ $to/{}"
+function copy()
+{
+    # define function to avoid limitation of xargs replacement up to 255 bytes
+    class=$1
+    from=$2
+    to=$3
+    count=$4
+    find $from -maxdepth 3 -name "${class}@*" | shuf | head -n $count | xargs -I{} cp {} $to/$class/
+}
+
+export -f copy
+
+cat $classlist | xargs -I{} bash -c "mkdir -p $to/{}"
+cat $classlist | xargs -n1 -I{} -P8 bash -c "copy {} $from $to $count"
+#head -n $count | xargs -I@ -P8 echo @ $to/{}"
