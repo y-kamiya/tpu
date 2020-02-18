5f25054bfda35fa891eddc8f2a56e0e3ae241527
Yuji Kamiya <y.kamiya0@gmail.com>
2020-02-18 10:30:58 +0900

    build tfrecords with python2.7

--- scripts/build_tfrecords.sh
+++ scripts/build_tfrecords.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 ROOT_DIR=$(cd $1 && pwd)
 DATASET_DIR=$2
@@ -13,7 +13,12 @@ fi
 output_dir=$DATASET_DIR/tf_records
 dataset_name=$(basename $DATASET_DIR)
 
-docker run -v $ROOT_DIR:/root -w /root -it tensorflow/tensorflow:1.14.0 bash -c "python /root/tools/datasets/imagenet_to_gcs.py  --raw_data_dir /root/$DATASET_DIR --local_scratch_dir /root/$output_dir --nogcs_upload"
+docker run -v $ROOT_DIR:/root -w /root -it tensorflow/tensorflow:1.15.0 bash -c "python /root/tools/datasets/imagenet_to_gcs.py  --raw_data_dir /root/$DATASET_DIR --local_scratch_dir /root/$output_dir --nogcs_upload"
+if [ $? -ne 0 ]; then
+    pip2.7 install tensorflow==1.15.0
+    python2.7 $ROOT_DIR/tools/datasets/imagenet_to_gcs.py  --raw_data_dir $DATASET_DIR --local_scratch_dir $output_dir --nogcs_upload
+fi
+exit
 
 gsutil -m cp -r $output_dir/train/train-*  gs://${PROJECT_ID}-vcm/$dataset_name/tf_records/
 gsutil -m cp -r $output_dir/validation/validation-*  gs://${PROJECT_ID}-vcm/$dataset_name/tf_records/
